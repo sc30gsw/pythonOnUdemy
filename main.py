84be746bd9eaa6934f3d537c3dcf2aeddb322224
sc30gsw <haidoushi875@gmail.com>
2022-09-18 09:41:30 +0900

    セレクトボックスによるインタラクティブ（双方向）な操作の実装

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ st.write('Display Image')
 
 # チェックボックスの表示
 # チェック時に画像を表示
-if st.checkbox('Show Image'):
+# if st.checkbox('Show Image'):
   # 画像の読み込み
-  img = Image.open('icon.png')
+  # img = Image.open('icon.png')
   # 画像の表示
-  st.image(img, caption='柴犬', use_column_width=False)
+  # st.image(img, caption='柴犬', use_column_width=False)
+
+# セレクトボックスの表示
+option = st.selectbox(
+  'あなたが好きな数字を教えてください。',
+  list(range(1, 11))
+)
+# セレクトしたものを表示
+'あなたの好きな数字は、', option, 'です。'
